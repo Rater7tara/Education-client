0d28e2e1d34bc062049cb0edf238428ba2c11ba3
Rater7tara <nowshinakteremu005@gmail.com>
2023-12-06 00:32:01 +0600

    navbar add

--- src/components/Shared/NavBar/NavBar.jsx
+++ src/components/Shared/NavBar/NavBar.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './NavBar.css';
 
 const NavBar = () => {
+    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+    const handleNavCollapse = () => {
+        setIsNavCollapsed(!isNavCollapsed);
+    };
+
     return (
-        <div>
-            
-        </div>
+        <nav className="navbar">
+            <button className="nav-toggle" onClick={handleNavCollapse}>
+                &#9776;
+            </button>
+            <div className={`nav-bar ${isNavCollapsed ? 'collapsed' : ''}`}>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Schedules</a>
+                <a href="#">Membership</a>
+                <a href="#">Pricing</a>
+            </div>
+
+            <div className="nav-bar right">
+                <a href="#">Offers</a>
+                <a href="#">Courses</a>
+            </div>
+        </nav>
     );
 };
 
