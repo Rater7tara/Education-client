7c7fecae70b24a1782dbc40dd2714ebc95353765
Rater7tara <nowshinakteremu005@gmail.com>
2023-12-06 13:18:58 +0600

    banner added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,9 +3,24 @@ import './Banner.css';
 
 const Banner = () => {
     return (
-        <div>
-            
+        <div className='banner-container'>
+            <div className='banner-main'>
+                <div className='banner-text'>
+                    <h4>Discover the beauty of tropics</h4>
+                    <h1>Tropical <br /> Destinations</h1>
+                    <h2>For Students</h2>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Labore repellat quibusdam maxime eos molestiae deserunt?</p>
+
+                    <button className='button-banner'>Sign Up</button>
+                </div>
+
+                <div className='card banner-img'>
+                    <img className='img-banner' src="https://iili.io/JI5aTdb.png" alt="" />
+                </div>
+            </div>
         </div>
+
     );
 };
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 import Footer from '../Shared/Footer/Footer';
 import NavBar from '../Shared/NavBar/NavBar';
 
@@ -6,6 +7,7 @@ const Home = () => {
     return (
         <div>
             <NavBar></NavBar>
+            <Banner></Banner>
             <Footer></Footer>
         </div>
     );
